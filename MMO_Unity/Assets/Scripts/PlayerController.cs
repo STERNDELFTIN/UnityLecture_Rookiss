ac0daaa67c678316406f063050705e77d3cffd3b
DELFTIN <resendo13@gmail.com>
2024-03-07 01:29:53 +0900

    좌표 맞춰주기
    플레이어의 앞뒤로 움직이지 않는 문제를 해결하기 위해 좌표를 맞춰줌

--- MMO_Unity/Assets/Scripts/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/PlayerController.cs
@@ -16,13 +16,15 @@ public class PlayerController : MonoBehaviour
         // PlayerController (*)
     void Update()
     {
+        // Local -> World
+        // TransformDirection
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
-        if (Input.GetKey(KeyCode.A))
-            transform.position += Vector3.back * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
-            transform.position += Vector3.right * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.D))
-            transform.position += Vector3.left * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
+        if (Input.GetKey(KeyCode.A))
+            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
     }
 }
